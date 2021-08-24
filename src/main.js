637ea849c4bcfa55aed13a4950378def6b6c43ff
Louie <sky85245630@gmail.com>
2021-08-24 15:27:10 +0800

    update

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
 import axios from "axios";
 import VueAxios from "vue-axios";
+import "video.js/dist/video-js.css";
 
 Vue.use(ElementUI);
 Vue.config.productionTip = false;
--- src/views/live.vue
+++ src/views/live.vue
@@ -10,34 +10,47 @@
         </el-row>
 
         <div class="mt20" style="text-align: left;">
-            <el-button size="small" type="danger" round>澳門六合彩</el-button>
+            <el-button size="small" type="danger" round>{{
+                currentInfo.LotteryName
+            }}</el-button>
         </div>
 
         <div class="mt30">
-            <el-carousel height="423px">
-                <el-carousel-item v-for="item in imgUrl" :key="item">
-                    <img :src="item" alt="" style="width:100%" />
-                </el-carousel-item>
-            </el-carousel>
+            <video
+                id="my-video"
+                class="video-js vjs-default-skin"
+                controls
+                muted
+                preload="none"
+            >
+                <source
+                    src="https://bbb.jutianx.com/kkk/bbb.m3u8"
+                    type="application/x-mpegURL"
+                />
+            </video>
         </div>
 
         <div class="mt30">
             <el-row>
-                <el-col :span="10" style="text-align:left;line-height: 2;"
+                <el-col :span="8" style="text-align:left;line-height: 2;"
                     >開獎視頻</el-col
                 >
 
-                <el-col :span="14"
-                    ><el-input
+                <el-col :span="16">
+                    <el-input
                         size="small"
-                        v-model="input"
+                        v-model="search"
                         placeholder="请输入期號"
-                        style="width: 120px;margin-right:10px"
-                    ></el-input
-                    ><el-button size="small" type="primary"
-                        >搜尋</el-button
-                    ></el-col
-                >
+                        style="width: 100px;margin-right:10px"
+                    >
+                    </el-input>
+                    <el-button size="small" type="primary" @click="toSearch">
+                        搜尋
+                    </el-button>
+                    <el-button size="small" type="warning" @click="reset">
+                        重置
+                    </el-button>
+                </el-col>
             </el-row>
 
             <el-row class="mt20" style="text-align:left"
@@ -46,7 +59,7 @@
                         size="small"
                         round
                         :type="year == 0 ? 'danger' : ''"
-                        @click="year = 0"
+                        @click="getHistoryOpenInfo(0)"
                         >今年</el-button
                     >
 
@@ -54,7 +67,7 @@
                         size="small"
                         :type="year == 1 ? 'danger' : ''"
                         round
-                        @click="year = 1"
+                        @click="getHistoryOpenInfo(1)"
                         >去年</el-button
                     ></el-col
                 ></el-row
@@ -63,72 +76,273 @@
                 <el-col
                     class="mt50 liveElPadding"
                     :span="12"
-                    v-for="e in 8"
-                    :key="e"
+                    v-for="(e, index) in historyOpenInfo"
+                    :key="index"
                 >
-                    <img class="liveWidth" src="@/assets/logo.png" alt="" />
+                    <img
+                        class="liveWidth"
+                        src="@/assets/logo.png"
+                        alt=""
+                        @click="toPlay(e)"
+                    />
                     <div class="mt20">
-                        澳門六合彩 第<span class="live_red"> 2021206 </span>期
-                        開獎視頻
+                        {{ currentInfo.LotteryName }} 第<span class="live_red">
+                            {{ e.Issue }} </span
+                        >期 開獎視頻
                     </div>
                 </el-col>
             </el-row>
-            <el-pagination
-                class="mt50"
-                background
-                layout="prev, pager, next"
-                :total="1000"
+
+            <el-dialog
+                :title="
+                    currentInfo.LotteryName + ' 第' + toPlayData.Issue + '期'
+                "
+                center
+                :visible.sync="dialogVisible"
+                width="100%"
+                class="home_left"
             >
-            </el-pagination>
+                <el-row style="text-align: center;">
+                    <el-col :span="24">
+                        <span class="fz16"
+                            >開獎時間：{{ toPlayData.OpenTime }}</span
+                        >
+                    </el-col>
+                </el-row>
+                <br />
+                <el-row style="text-align: center;">
+                    <el-col :span="24">
+                        <span class="toPlay_kj fz16">開獎號碼：</span>
+                        <div class="num_sm" style="margin-top: 10px;">
+                            <span>
+                                <span
+                                    class="ball"
+                                    v-for="(q,
+                                    index) in toPlayData.OpenCode.split(',')"
+                                    :key="index"
+                                    :class="
+                                        q
+                                            .split(',')
+                                            .map((e) => color_list[e % 6])
+                                    "
+                                >
+                                    <span class="balls">{{ q }}</span>
+                                    <span class="shengxiao">{{
+                                        data_list[q % 12]
+                                    }}</span>
+                                </span>
+                            </span>
+                        </div>
+                    </el-col>
+                </el-row>
+
+                <el-row>
+                    <el-col :span="24" class="mt30">
+                        <video
+                            width="100%"
+                            height="100%"
+                            controls
+                            :src="require('@/video/2021067.mp4')"
+                        ></video>
+                    </el-col>
+                </el-row>
+            </el-dialog>
         </div>
     </div>
 </template>
 
 <script>
+import videojs from "video.js";
+import "videojs-contrib-hls";
+// Vue.prototype.$video = Video;
+
 export default {
-    name: "Post",
+    name: "Live",
     components: {},
+    watch: {
+        "$store.state.kj_day": function(e) {
+            // plan1.0
+            // console.log("statttt", e);
+            if (e) {
+                console.log("监听到kj_day");
+                this.getCurrentInfo();
+            } else {
+                this.msg = "监听不到";
+            }
+        },
+    },
     data() {
         return {
-            imgUrl: [
-                "https://mcjccdn-qq.goluosi.com/macaujc/pc/img/swiper1.jpg",
-            ],
-            input: "",
+            search: "",
             year: 0,
-            tableData: [
-                {
-                    issue: "2021206",
-                    openTime: "2021-07-25 21:34:05",
-                    openCode: "43,41,33,42,05,13,21",
-                    videoUrl: "",
-                },
+            list: [
+                "鼠",
+                "牛",
+                "虎",
+                "兔",
+                "龙",
+                "蛇",
+                "马",
+                "羊",
+                "猴",
+                "鸡",
+                "狗",
+                "猪",
             ],
-            color: {
-                red: "01,02,07,08,12,13,18,19,23,24,29,30,34,35,40,45,46",
-                green: "05,06,11,16,17,21,22,27,28,32,33,38,39,43,44,49",
-                blue: "03,04,09,10,14,15,20,25,26,31,36,37,41,42,47,48",
+            data_list: [],
+            color_list: ["red", "red", "green", "green", "blue", "blue"],
+            draw_num: "",
+            currentInfo: {},
+            issueOpenInfo: {},
+            historyOpenInfo: [],
+            dialogVisible: false,
+            toPlayData: {
+                Issue: "2021067",
+                LotteryId: 2032,
+                OpenCode: "44,13,26,34,35,14,12",
+                OpenTime: "2021-08-07 21:30:00",
+                Pet: "牛",
+                VideoUrl: "/video/2021067.mp4",
             },
-            animal: [
-                { mouse: "02,14,26,38" },
-                { ox: "01,13,25,37,49" },
-                { tiger: "12,24,36,48" },
-                { rabbit: "11,23,35,47" },
-                { dragon: "10,22,34,46" },
-                { snake: "09,21,33,45" },
-                { house: "08,20,32,44" },
-                { sheep: "07,19,31,43" },
-                { monkey: "06,18,30,42" },
-                { chicken: "05,17,29,41" },
-                { dog: "04,16,28,40" },
-                { pig: "03,15,27,39" },
-            ],
-            draw_num: "43,41,33,42,05,13,21",
         };
+    },
+    methods: {
+        initVideo() {
+            videojs(
+                "my-video",
+                {
+                    bigPlayButton: false,
+                    textTrackDisplay: false,
+                    posterImage: true,
+                    errorDisplay: false,
+                    controlBar: true,
+                    hls: {
+                        withCredentials: true,
+                    },
+                },
+                function() {
+                    this.play();
+                }
+            );
+        },
+        destroyVideo() {
+            videojs(
+                "my-video",
+                {
+                    bigPlayButton: false,
+                    textTrackDisplay: false,
+                    posterImage: true,
+                    errorDisplay: false,
+                    controlBar: true,
+                    hls: {
+                        withCredentials: true,
+                    },
+                },
+                function() {
+                    this.dispose();
+                }
+            );
+        },
+        getCurrentInfo() {
+            // console.log("env", process.env.VUE_APP_BASE_DOMAIN);
+            let url = `${process.env.VUE_APP_BASE_DOMAIN}/api/CurrentInfo`;
+            // var url = "http://localhost:81/api/CurrentInfo";
+            // var url = "http://localhost:81/api/is_kj_day";
+            //一開始沒有抓到state資料會預設no
+            let data = { is_hk: this.$store.state.kj_day };
+            // if(!this.$store.state.kj_day){
+            // console.log(typeof this.$store.state.kj_day);
+            // }
+            // console.log("store", this.$store.state.kj_day);
+            this.axios.post(url, data).then((res) => {
+                let { data } = res.data;
+                this.currentInfo = data;
+            });
+            // console.log("this.$store.state.kj_day", this.$store.state.kj_day);
+            this.getIssueOpenInfo();
+            this.getHistoryOpenInfo();
+        },
+        getIssueOpenInfo() {
+            let url = `${process.env.VUE_APP_BASE_DOMAIN}/api/IssueOpenInfo`;
+            let data = {
+                lottery: this.currentInfo.LotteryId,
+                is_hk: this.$store.state.kj_day,
+            };
+            // console.log("getIssueOpenInfo", data);
+            this.axios.post(url, data).then((res) => {
+                let { data } = res.data;
+                this.issueOpenInfo = data;
+                this.initData();
+            });
+        },
+        getHistoryOpenInfo(e) {
+            let today = new Date();
+            // 今年
+            if (e == 0) {
+                this.year = 0;
+                today = today.toISOString().substring(0, 10);
+            }
+            //去年
+            if (e == 1) {
+                this.year = 1;
+                today = today.toISOString().substring(0, 4) - 1 + "-12-31";
+            }
+            let url = `${process.env.VUE_APP_BASE_DOMAIN}/api/HistoryOpenInfoList`;
+            let data = {
+                lottery: this.currentInfo.LotteryId,
+                issueNum: today,
+                is_hk: this.$store.state.kj_day,
+            };
+            // console.log("getHistoryOpenInfo", data);
+            this.axios.post(url, data).then((res) => {
+                let { data } = res.data;
+                this.historyOpenInfo = data;
+                this.searchHistoryOpenInfo = data;
+            });
+        },
+        timeElapsedHandler() {},
+        initData() {
+            // console.log("initData", this.issueOpenInfo);
+            this.draw_num = this.issueOpenInfo.OpenCode.split(",");
+            let petQQ = this.currentInfo.Pet;
+            let index = this.list.indexOf(petQQ) - 1;
+            let front = this.list.slice(index);
+            let end = this.list.slice(0, index);
+            this.data_list = front.concat(end);
+        },
+        toSearch() {
+            // this.getHistoryOpenInfo();
+            this.historyOpenInfo = this.searchHistoryOpenInfo.filter((e) => {
+                return e.Issue.includes(this.search);
+            });
+            // console.log("search", this.historyOpenInfo.search(this.search));
+        },
+        reset() {
+            this.historyOpenInfo = this.searchHistoryOpenInfo;
+            this.search = "";
+        },
+        toPlay(e) {
+            console.log("e", e);
+            this.dialogVisible = true;
+            this.toPlayData = e;
+            // this.video = "@" + e.row.VideoUrl;
+
+            // :src="require(`@${toPlayData.VideoUrl}`)"
+
+            // :src="require('@/video/2021067.mp4')"
+        },
+    },
+    mounted() {
+        this.getCurrentInfo();
+        this.initVideo();
+    },
+    beforeDestroy() {
+        this.destroyVideo();
     },
 };
 </script>
 
-<style>
+<style scoped>
 .home_box {
     margin-top: 20px;
     box-shadow: 0 1px 4px #c8c8c8;
@@ -199,5 +413,17 @@ export default {
 
 .liveElPadding {
     padding: 10px;
+}
+</style>
+
+<style>
+.video-js[tabindex="-1"] {
+    outline: none;
+    width: 100%;
+}
+
+.el-dialog__body {
+    text-align: initial;
+    padding: 25px 10px 10px !important;
 }
 </style>
